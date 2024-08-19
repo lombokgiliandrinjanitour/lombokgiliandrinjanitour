f03da777d8adcbafe8e4d4915fbdfa20c116b942
lombokgiliandrinjanitour <herususanto.st@gmail.com>
2024-08-19 21:34:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function order(packageName) {
-    const phoneNumber = '0817366355';
+    const phoneNumber = '62817366355';
     const message = `Halo, saya tertarik untuk memesan ${packageName}.`;
     const waUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(waUrl, '_blank');
